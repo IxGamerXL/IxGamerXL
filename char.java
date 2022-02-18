4eaa87aeccaf15c014647d14641ec5e7bde99d1a
IxGamerXL but creative <90928500+IxGamerXL@users.noreply.github.com>
2022-02-17 23:36:06 -0600

    Update char.java

--- char.java
+++ char.java
@@ -1,1 +1,25 @@
-
+public class charData {
+	
+	public static final name = "GSF";
+	
+	protected int health = 125;
+	protected int maxhealth = health;
+	protected int strength = 175;
+	
+	protected int trueStrength = strength * 15;
+	protected int determination = 215;
+	protected int creativity = 1 << 31;
+	
+	private lastMsg = "";
+	
+	public static void pm(String msg) {
+		System.out.println("-> " + name + ": " + msg);
+	}
+	
+	private static void reply(String msg) {
+		System.out.println("<- " + name + ": " + msg);
+	}
+	
+	// Not all data is visible to you... yet.
+	
+}
